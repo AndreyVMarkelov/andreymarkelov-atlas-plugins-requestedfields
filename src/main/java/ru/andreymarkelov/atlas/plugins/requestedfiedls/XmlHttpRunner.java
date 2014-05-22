3542c98224c3463c5c819c0d26af893bd8b72cff
Magosányi Árpád <armagosanyi@telenor.hu>
2014-05-22 15:51:07 +0200

    xpath 2.0, namespaces

--- src/main/java/ru/andreymarkelov/atlas/plugins/requestedfiedls/XmlHttpRunner.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/requestedfiedls/XmlHttpRunner.java
@@ -7,18 +7,18 @@ import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmItem;
+import net.sf.saxon.s9api.XdmNode;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.xml.dtm.ref.DTMNodeList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class XmlHttpRunner {
     private static final Logger log = LoggerFactory.getLogger(XmlHttpRunner.class);
@@ -48,24 +48,31 @@ public class XmlHttpRunner {
             List<String> vals = new ArrayList<String>();
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             factory.setNamespaceAware(true);
+            Processor proc = new Processor(false);
+            net.sf.saxon.s9api.DocumentBuilder s9builder = proc.newDocumentBuilder();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
-            XPathFactory xpathfactory = XPathFactory.newInstance();
-            XPath xpath = xpathfactory.newXPath();
-            XPathExpression expr = xpath.compile(data.getReqPath());
-            Object result = expr.evaluate(doc, XPathConstants.NODESET);
-            NodeList nodes = (NodeList) result;
-            for (int i = 0; i < nodes.getLength(); i++) {
-                Node node = nodes.item(i);
-                System.out.printf("xml:%s %s\n", node, node.getNodeType());
-                if (node.getNodeType() == Node.TEXT_NODE) {
-                    String nodeText = node.getTextContent();
-                    if (!StringUtils.isEmpty(nodeText)) {
-                        vals.add(nodes.item(i).getNodeValue());
-                    }
+            XdmNode s9doc = s9builder.wrap(doc);
+
+            XPathCompiler compiler = proc.newXPathCompiler();
+            
+            UniversalNamespaceCache nsCache = new UniversalNamespaceCache(doc,false);
+            
+            for(String nsname : nsCache.getAllPrefixes()) {
+                compiler.declareNamespace(nsname, nsCache.getNamespaceURI(nsname));
+            }
+            XPathExecutable expr = compiler.compile(data.getReqPath());
+            XPathSelector selector = expr.load();
+            selector.setContextItem(s9doc);
+            selector.evaluate();
+            for (XdmItem item : selector) {
+                System.out.printf("item=%s\n", item);
+                String nodeText = item.getStringValue();
+                if (!StringUtils.isEmpty(nodeText)) {
+                    vals.add(nodeText);
                 }
             }
-
+            
             if (defValue != null) {
                 vals.add(0, defValue.toString());
             }
@@ -78,8 +85,9 @@ public class XmlHttpRunner {
                 res.setVals(vals);
             }
         } catch (Throwable th) {
-            log.error("XmlHttpRunner::getData - error renderring", th);
+            log.error("XmlHttpRunner::getData - error rendering", th);
             res.setError(th.getMessage());
+            th.printStackTrace();
         }
 
         return res;
--- src/test/java/ru/andreymarkelov/atlas/plugins/requestedfiedls/XmlHttpRunnerTest.java
+++ src/test/java/ru/andreymarkelov/atlas/plugins/requestedfiedls/XmlHttpRunnerTest.java
@@ -34,7 +34,73 @@ public class XmlHttpRunnerTest {
                 )
         );
     }
+
+    @Test
+    public void Attribute_values_can_be_extracted() {
+        assertXmlRunnerFunctionality(
+                "<bar><foo id=\"id1\">result</foo><foo id=\"id2\">result2</foo></bar>",
+                "//@id",
+                Arrays.asList(
+                        "Default Value",
+                        "id1",
+                        "id2"
+                )
+        );
+    }
     
+    @Test
+    public void No_need_to_convert_node_contents_to_text() {
+        assertXmlRunnerFunctionality(
+                "<bar><foo>result</foo><foo>result2</foo></bar>",
+                "//foo",
+                Arrays.asList(
+                        "Default Value",
+                        "result",
+                        "result2"
+                )
+        );
+    }
+
+    @Test
+    public void Xpath_can_use_doc_namespaces() {
+        assertXmlRunnerFunctionality(
+                "<bar xmlns:baz=\"http://foo.dom/bar\"><baz:foo>result</baz:foo><foo>thisnot</foo><baz:foo>result2</baz:foo></bar>",
+                "//baz:foo",
+                Arrays.asList(
+                        "Default Value",
+                        "result",
+                        "result2"
+                )
+        );
+    }
+
+    @Test
+    public void Xpath_uses_doc_default_namespace() {
+        assertXmlRunnerFunctionality(
+                "<bar xmlns=\"http://foo.dom/bar\"><foo>result</foo><foo>result2</foo></bar>",
+                "//foo",
+                Arrays.asList(
+                        "Default Value",
+                        "result",
+                        "result2"
+                )
+        );
+    }
+
+    @Test
+    public void Xpath_processor_is_version_2() {
+        assertXmlRunnerFunctionality(
+                "<bar xmlns=\"http://foo.dom/bar\"><foo>result</foo><foo>result2</foo></bar>",
+                "for $i in //foo return concat($i,'--')",
+                Arrays.asList(
+                        "Default Value",
+                        "result--",
+                        "result2--"
+                )
+        );
+    }
+
+
     private void assertXmlRunnerFunctionality(String xml, String path,
             List<String> expected) {
         HttpSender mockSender = mock(HttpSender.class);
@@ -45,6 +111,6 @@ public class XmlHttpRunnerTest {
         XmlHttpRunner runner = new XmlHttpRunner(data, defValue, mockSender);
         HttpRunnerData returned = runner.getData();
         System.out.printf("returned: %s\n", returned);
-        assertEquals(returned.getVals(),expected);
+        assertEquals(expected, returned.getVals());
     }
 }
